d849466c5d2887726acb3c7248d29ac4dd6ac17d
BSVerdi <brenoverdi2005@gmail.com>
2025-02-04 13:20:34 -0300

    some changes

--- exercicios_beecrowd/7-Grafos/ex1123.c
+++ exercicios_beecrowd/7-Grafos/ex1123.c
@@ -2,85 +2,109 @@
 #include<stdlib.h>
 #include<limits.h>
 
+const int INF = INT_MAX;
 
-void dijkstra(int **grafo, int origem, int cidades, int cidades_na_rota) {
-    int distancia[cidades];
-    int visitado[cidades];
+typedef struct {
+    int data[1010];
+    int size;
+} Queue;
 
-    // inicializa 
+
+void push(Queue *fila, int data) {
+    fila->data[fila->size++] = data;
+}
+
+
+int pop(Queue *fila) {
+    int temp = fila->data[0];
+
+    for (int i = 0; i < fila->size - 1; i++)
+        fila->data[i] = fila->data[i + 1];
+
+    fila->size--;
+    return temp;
+}
+
+
+void infinito(int **grafo, int *custo, int cidades) {
     for (int i = 0; i < cidades; i++) {
-        distancia[i] = INT_MAX;
-        visitado[i] = 0;
+        custo[i] = INF;
+        for (int j = 0; j < cidades; j++)
+            grafo[i][j] = INF;
     }
+}
 
-    distancia[origem] = 0;
 
-    // processa todos os vertices
-    for (int i = 0; i < cidades; i++) {
-        // escolhe o no com a menor distancia
-        int u = -1;
+int dijkstra(int **grafo, int *custo, int cidades, int origem, int destino) {
+    Queue fila;
+    fila.size = 0;
+    
+    push(&fila ,origem);
+
+    custo[origem] = 0;
+
+    while (fila.size > 0) {
+        int i = pop(&fila);
+
         for (int j = 0; j < cidades; j++) {
-            if (!visitado[j] && (u == -1 || distancia[j] < distancia[u]))
-                u = j;
-        }
-
-        if (u == -1 || distancia[u] == INT_MAX) break;
-        visitado[u] = 1; //marca o no como visitado;
-
-        // atualiza a distancia dos nos adjacentes
-        for (int v = 0; v < cidades; v++) {
-            if (grafo[u][v] > 0) {
-                if (u >= cidades_na_rota || v >= cidades_na_rota || v == u + 1) {
-                    if (distancia[u] + grafo[u][v] < distancia[v]) {
-                        distancia[v] = distancia[u] + grafo[u][v];
-                    }
-                }
+            if (grafo[i][j] != INF && custo[j] > custo[i] + grafo[i][j]) {
+                custo[j] = custo[i] + grafo[i][j];
+                push(&fila, j);
             }
         }
-    } 
-
-    // exibe as distancia
-    if (cidades_na_rota == 0)
-        printf("%d\n", distancia[cidades_na_rota]);
-    else
-        printf("%d\n", distancia[cidades_na_rota - 1]);
+    }
+    
+    return custo[destino];
 }
 
 
 int main() {
-    int **grafo, cidades = 1, estradas = 1, cidades_na_rota = 1, conserto = 1, i, j;
-    
+    int cidades, estradas, cidades_rota, conserto;
 
     while (1) {
-        // quantidade de cidades e estradas
-        scanf("%d %d %d %d", &cidades, &estradas, &cidades_na_rota, &conserto);
+        scanf("%d %d %d %d", &cidades, &estradas, &cidades_rota, &conserto);
 
-        if (!(cidades || estradas || cidades_na_rota || conserto)) break;    
+        if (!(cidades || estradas || cidades_rota || conserto)) break;
 
-        grafo = (int**)malloc(sizeof(int*) * cidades);
-        for (i=0; i<cidades; i++)
-            grafo[i] = (int*)malloc(sizeof(int) * cidades);
+        int **grafo;
+        int *custo;
 
-        for (i=0; i<cidades; i++) {
-            for(j=0; j<cidades; j++)
-                grafo[i][j] = 0;
+        grafo = (int**)malloc(cidades * sizeof(int*));
+        for (int i = 0; i < cidades; i++)
+            grafo[i] = (int*)malloc(cidades * sizeof(int));
+
+        custo = (int*)malloc(cidades * sizeof(int));
+
+        infinito(grafo, custo, cidades);
+
+        for (int i = 1; i <= estradas; i++) {
+            int u, v, p;
+
+            scanf("%d %d %d", &u, &v, &p);
+
+            if (u >= cidades_rota && v >= cidades_rota) {
+                grafo[u][v] = p;
+                grafo[v][u] = p;
+            }
+
+            if (u >= cidades_rota && v < cidades_rota)
+                grafo[u][v] = p;
+
+            if (u < cidades_rota && v >= cidades_rota)
+                grafo[v][u] = p;
+
+            if (u < cidades_rota && v < cidades_rota && abs(u - v) == 1) {
+                grafo[u][v] = p;
+                grafo[v][u] = p;
+            }
         }
 
-        for (int k = 0; k < estradas; k++) {
-            int temp;
+        printf("%d\n", dijkstra(grafo, custo, cidades, conserto, cidades_rota -1));
 
-            scanf("%d %d",&i, &j);
-            scanf("%d", &temp);
-
-            grafo[i][j] = temp;
-            grafo[j][i] = temp;
-        }
-
-        dijkstra(grafo, conserto, cidades, cidades_na_rota);
-        
-        for (i = 0; i < cidades; i++)
+        for (int i = 0; i < cidades; i++)
             free(grafo[i]);
         free(grafo);
+        free(custo);
     }
 
     return 0;
